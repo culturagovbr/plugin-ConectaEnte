7b5b2a62af546ecf21392719ec8085251e2c7f93
Junior-Shyko <franciscoanto@gmail.com>
2026-09-19 16:37:40 -0300

    #3 Criando controller para rota e renderizando view

--- Plugin.php
+++ Plugin.php
@@ -3,6 +3,7 @@
 namespace ConectaEnte;
 
 use AldirBlanc\Services\UserAccessService;
+use ConectaEnte\Controllers\ConectaEnteController;
 use MapasCulturais\i;
 use MapasCulturais\App;
 
@@ -12,9 +13,7 @@ class Plugin extends \MapasCulturais\Plugin
     public function _init(){
         $app = App::i();
 
-        $canAccess = UserAccessService::canAccess();
-
-        $app->hook('panel.nav', function (&$nav) use ($canAccess) {
+        $app->hook('panel.nav', function (&$nav) {
 
             $nav['more']['condition'] = fn() => UserAccessService::isSaasSuperAdmin();
 
@@ -29,7 +28,7 @@ class Plugin extends \MapasCulturais\Plugin
                 };
 
                 $nav['admin']['items'][] = [
-                    'route' => 'panel/federativeEntities',
+                    'route' => 'conectaEnte/federativeEntities',
                     'icon' => 'agent',
                     'label' => i::__('Entes Federados'),
                     'condition' => fn() => UserAccessService::isSaasSuperAdmin(),
@@ -42,94 +41,12 @@ class Plugin extends \MapasCulturais\Plugin
                     'condition' => fn() => UserAccessService::isSaasSuperAdmin(),
                 ];
             }
-
-            if (!$canAccess && isset($nav['opportunities']['items'])) {
-                foreach ($nav['opportunities']['items'] as $key => $item) {
-                    if (isset($item['route']) && $item['route'] === 'panel/opportunities') {
-                        $nav['opportunities']['items'][$key]['condition'] = fn() => false;
-                    }
-                }
-            }
-
-            foreach ($nav as &$group) {
-                if (isset($group['items'])) {
-                    foreach ($group['items'] as &$item) {
-                        if (isset($item['route'])) {
-                            if (in_array($item['route'], ['panel/submissions', 'panel/registrations', 'submissions', 'registrations'])) {
-                                $item['icon'] = 'registration';
-                            } elseif (in_array($item['route'], ['panel/evaluations', 'evaluations'])) {
-                                $item['icon'] = 'evaluation';
-                            } elseif (in_array($item['route'], ['panel/validations', 'validations'])) {
-                                $item['icon'] = 'validation';
-                            }
-                        }
-                    }
-                    unset($item);
-                }
-            }
-            unset($group);
-
-            if (!UserAccessService::isGestorCultBr()) {
-                return;
-            }
-
-            if (UserAccessService::isSaasSuperAdmin()) {
-                return;
-            }
-
-            $nav['admin']['condition'] = fn() => false;
-
-            foreach ($nav['opportunities']['items'] as $key => $item) {
-                if (isset($item['route']) && $item['route'] === 'panel/opportunities') {
-                    $nav['opportunities']['items'][$key]['condition'] = fn() => false;
-                }
-            }
-
-            if (isset($nav['opportunities']['items'])) {
-                foreach ($nav['opportunities']['items'] as $key => $item) {
-                    if (isset($item['route']) && $item['route'] === 'panel/validations') {
-                        $nav['opportunities']['items'][$key]['condition'] = fn() => false;
-                    }
-                }
-            }
-
-            $nav['federativeEntity'] = [
-                'condition' => fn() => true,
-                'label' => i::__('Ente Federado'),
-                'items' => [
-                    [
-                        'route' => 'panel/opportunities',
-                        'icon' => 'opportunity',
-                        'label' => i::__('Oportunidades'),
-                    ],
-                    [
-                        'route' => 'panel/federativeEntityAgents',
-                        'icon' => 'agent',
-                        'label' => i::__('Minha Equipe'),
-                    ],
-                    [
-                        'route' => 'panel/validations',
-                        'icon' => 'validation',
-                        'label' => i::__('Minhas Validações'),
-                    ]
-                ],
-            ];
         });
     }
 
     function register(){
         $app = App::i();
 
-        $def = new \MapasCulturais\Definitions\Role(
-            'GestorCultBr',
-            i::__('Gestor CultBR'),
-            i::__('Gestor CultBR'),
-            true,
-            function (\MapasCulturais\UserInterface $user, $subsite_id) {
-                return false;
-            },
-            [],
-        );
-        $app->registerRole($def);
+        $app->registerController('conectaEnte', ConectaEnteController::class);
     }
 }
