fe7eb6a22b2d396963998b97998aa034d771509b
Junior-Shyko <franciscoanto@gmail.com>
2026-09-19 17:49:43 -0300

    #3 Removendo botao sem funcionalidade

--- views/conectaente/federative-entities.php
+++ views/conectaente/federative-entities.php
@@ -10,18 +10,12 @@ $this->import('panel--entity-tabs panel--entity-card mc-icon create-space');
         <div class="panel-page__header-title">
             <div class="title">
                 <div class="title__icon space__background"> <mc-icon name="space"></mc-icon> </div>
-                <h1 class="title__title"> <?= i::_e('Entes Federais') ?> </h1>
+                <h1 class="title__title"> <?= i::_e('Entes Federados') ?> </h1>
             </div>
         </div>
         <p class="panel-page__header-subtitle">
-            <?= i::_e('Nesta seção você pode adicionar e gerenciar todos os entes federais') ?>
+            <?= i::_e('Nesta seção você pode adicionar e gerenciar todos os entes federados') ?>
         </p>
-        <div class="panel-page__header-actions">
-            <button @click="modal.open()" class="button button--primary button--icon">
-                <mc-icon name="add"></mc-icon>
-                <span><?= i::__('Criar Ente Federal') ?></span>
-            </button>
-        </div>
     </header>
 
     <panel--entity-tabs type="space"></panel--entity-tabs>
