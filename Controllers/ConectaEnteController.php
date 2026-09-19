aca23af8db8ef3cf4804df16cdaa8cd4932a1259
Junior-Shyko <franciscoanto@gmail.com>
2026-09-19 17:40:06 -0300

    #3 Retornando PermissionDenied

--- Controllers/ConectaEnteController.php
+++ Controllers/ConectaEnteController.php
@@ -2,6 +2,10 @@
 
 namespace ConectaEnte\Controllers;
 
+use AldirBlanc\Services\UserAccessService;
+use MapasCulturais\App;
+use MapasCulturais\Exceptions\PermissionDenied;
+
 class ConectaEnteController extends \MapasCulturais\Controller
 {
     function __construct()
@@ -12,6 +16,11 @@ class ConectaEnteController extends \MapasCulturais\Controller
     function GET_federativeEntities()
     {
         $this->requireAuthentication();
+
+        if (!UserAccessService::isSaasSuperAdmin()) {
+            throw new PermissionDenied(App::i()->user);
+        }
+
         $this->render('federative-entities');
     }
 }
