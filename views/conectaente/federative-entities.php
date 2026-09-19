1d6803b0fe8299247f0da8aeb6f8b2427d73e02e
Junior-Shyko <franciscoanto@gmail.com>
2026-09-19 18:48:49 -0300

    #3 Melhorando o codigo do icone

--- views/conectaente/federative-entities.php
+++ views/conectaente/federative-entities.php
@@ -9,7 +9,7 @@ $this->import('panel--entity-tabs');
     <header class="panel-page__header">
         <div class="panel-page__header-title">
             <div class="title">
-                <div class="title__icon space__background"> <mc-icon name="space"></mc-icon> </div>
+                <div class="title__icon space__background"> <mc-icon name="agent"></mc-icon> </div>
                 <h1 class="title__title"> <?= i::_e('Entes Federados') ?> </h1>
             </div>
         </div>
