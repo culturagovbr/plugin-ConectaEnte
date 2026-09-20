120676d65a7cfa0c2cac61b2fcafc07ee827bdf8
Lucas Oliveira <lucas.fullstack.dev@gmail.com>
2026-09-20 16:35:59 -0300

    feat: busca da listagem aceita CNPJ sem pontuação

--- components/conectaente--entities-list/script.js
+++ components/conectaente--entities-list/script.js
@@ -21,6 +21,7 @@ app.component('conectaente--entities-list', {
     computed: {
         visibleEntities() {
             const keyword = this.keyword.trim().toLocaleLowerCase();
+            const digits = keyword.replace(/\D/g, '');
 
             if (!keyword) {
                 return this.entities;
@@ -28,9 +29,11 @@ app.component('conectaente--entities-list', {
 
             return this.entities.filter((entity) => {
                 const name = `${entity.name || ''}`.toLocaleLowerCase();
-                const document = `${entity.document || ''}`.toLocaleLowerCase();
+                const document = `${entity.document || ''}`;
 
-                return name.includes(keyword) || document.includes(keyword);
+                return name.includes(keyword)
+                    || document.includes(keyword)
+                    || (digits.length > 0 && document.replace(/\D/g, '').includes(digits));
             });
         },
     },
